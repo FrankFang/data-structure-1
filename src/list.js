57311f5cec4d512f57932e0dcc2dc049ee04a6d3
Frank Fang <FrankFang1990@gmail.com>
2019-11-29 11:49:51 +0800

    添加注释

--- src/list.js
+++ src/list.js
@@ -13,7 +13,7 @@ const appendList = (list, value) => {
 };
 const removeFromList = (list, node) => {
   let x = list;
-  let p = null;
+  let p = null; // 这里将 p 初始化为 null，这句话没有特殊目的
   while (x !== node && x !== null) { // 课堂里忘了对 null 进行处理，如果 node 不在 list 中，x 就可能为 null
     p = x;
     x = x.next;
